1ff7d26166fbdff312826ca06ca2b0756fed632d
Ajeet Singh <Ajeetsingh82900@gmail.com>
2024-01-07 00:19:03 +0530

    Todoitem1 component created

--- src/Components/Todoitem1/index.jsx
+++ src/Components/Todoitem1/index.jsx
@@ -1,27 +1,19 @@
-import Todoitem1 from "./Components/Todoitem1";
-
-function App() {
+export default function Todoitem1(){
   return (
-    <center class="todo-container">
-      <h1>Todo App</h1>
-      <div class="container text-center">
-        <Todoitem1></Todoitem1>
-        
-        
+    <div class="container text-center">
+        <div class="row">
         <div class="row">
           <div class="col-6">
             Buy Milk
           </div>
           <div class="col-4">
-           16/12/2024
+            12/03/2015
           </div>
           <div class="col-2">
           <button type="button" class="btn btn-danger">Danger</button>
           </div>
         </div>
-      </div>
-    </center>
+        </div>
+        </div>
   );
 }
-
-export default App;
