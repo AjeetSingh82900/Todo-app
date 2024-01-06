0c0244bc85749e9715fbf5ccd29941bbcb3245fe
Ajeet Singh <Ajeetsingh82900@gmail.com>
2024-01-06 22:41:32 +0530

    staging

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+
+
+function App() {
+  return (
+    <center class="todo-container">
+      <h1>Todo App</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter Todo Here"/>
+          </div>
+          <div class="col-4">
+            <input type ="date"/>
+          </div>
+          <div class="col-2">
+          <button type="button" class="btn btn-success">Add</button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-6">
+            Buy Milk
+          </div>
+          <div class="col-4">
+            12/03/2015
+          </div>
+          <div class="col-2">
+          <button type="button" class="btn btn-danger">Danger</button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-6">
+            Buy Milk
+          </div>
+          <div class="col-4">
+           16/12/2024
+          </div>
+          <div class="col-2">
+          <button type="button" class="btn btn-danger">Danger</button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
+}
+
+export default App;
