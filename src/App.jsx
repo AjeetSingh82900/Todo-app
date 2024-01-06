8056736efa6cf1b95a6b0cdf2df4164b8dfe73e1
Ajeet Singh <Ajeetsingh82900@gmail.com>
2024-01-07 00:00:41 +0530

    Apptodo component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-
+import Apptodo from './Components/Todobutton/Apptodo';
 
 function App() {
   return (
     <center class="todo-container">
       <h1>Todo App</h1>
+      <Apptodo />
       <div class="container text-center">
-        <div class="row">
-          <div class="col-6">
-            <input type="text" placeholder="Enter Todo Here"/>
-          </div>
-          <div class="col-4">
-            <input type ="date"/>
-          </div>
-          <div class="col-2">
-          <button type="button" class="btn btn-success">Add</button>
-          </div>
-        </div>
+        
+        
         <div class="row">
           <div class="col-6">
             Buy Milk
